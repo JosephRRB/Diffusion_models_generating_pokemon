1695831a64af3c047a6889f1c8ecb4c4951f61fb
Joseph Bunao <jbunao@aim.edu>
2023-03-09 15:36:34 -0500

    Initial commit

---
+++ core/utils.py
@@ -0,0 +1,23 @@
+import glob
+
+import matplotlib.pyplot as plt
+
+from torchvision.io import read_image, ImageReadMode
+from torch.utils.data import Dataset
+
+
+# Dataset cloned from: https://github.com/HybridShivam/Pokemon
+class ImageDataset(Dataset):
+    def __init__(self, image_directory="images/", transform_fn=None):
+        self.image_filenames = sorted(glob.glob(f"{image_directory}*"))
+        self.transform_fn = transform_fn
+
+    def __len__(self):
+        return len(self.image_filenames)
+
+    def __getitem__(self, index):
+        image_path = self.image_filenames[index]
+        image = read_image(image_path, mode=ImageReadMode.RGB)
+        if self.transform_fn:
+            image = self.transform_fn(image)
+        return image
